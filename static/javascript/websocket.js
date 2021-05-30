880f029d700fff4cb53b7393ef684264f36ad5be
Joshua-Zou <joshyzou@gmail.com>
2021-05-30 13:04:41 -0700

    FIXED FOUC!!!

--- static/javascript/websocket.js
+++ static/javascript/websocket.js
@@ -1,12 +1,18 @@
 document.getElementById("serverTime").innerHTML = "cannot reach server at this time"
+var version = detectIE();
+if (version !== false){
+  var source = new EventSource('/sse/server-time');
 
-var source = new EventSource('/sse/server-time');
+  // handle messages
+  source.onmessage = function(event) {
+      console.log(event.data);
+      document.getElementById("serverTime").innerHTML = event.data
+  };
+  source.onclose = function (event) {
+    console.log(event)
+  }
+}else document.getElementById("serverTime").innerHTML = "cannot reach server at this time"
 
-// handle messages
-source.onmessage = function(event) {
-    console.log(event.data);
-    document.getElementById("serverTime").innerHTML = event.data
-};
         let theme = localStorage.getItem('theme');
         if (theme === "dark"){
           let slider = document.getElementById("bigTheme");
@@ -36,4 +42,24 @@ function sleep(ms) {
   function signout() {
     console.log('User signed out.');
    window.location.href = "/destroySession"
+      }
+      function detectIE() {
+        var ua = window.navigator.userAgent;
+      
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+          return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+      
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+          var rv = ua.indexOf('rv:');
+          return 11;//parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+      
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+          return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+        return false;
       }
